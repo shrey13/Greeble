20152c6119312fd7931cb45d33b6f339d191530c
Sudhanshu Mishra <snmishra@mit.edu>
2015-04-23 11:23:32 -0400

    Creating Parent View

---
+++ Greeble/ParentViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PARENTViewController.swift
+//  Greeble
+//
+//  Created by Sudhanshu Nath Mishra on 4/23/15.
+//  Copyright (c) 2015 Greeblers. All rights reserved.
+//
+
+import UIKit
+
+class ParentViewController: UIViewController {
+
+    @IBOutlet var tasksTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
